300ae14b992c7534fdf4c97af967330052339c2d
Valentina Gómez <valengogu96@gmail.com>
2025-05-19 18:50:55 +0000

    termiado proyecto

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 const newCard = () => {
-  console.log('genero una nueva carta')
-
   let suits = ['♠', '♥', '♦', '♣'];
   let numbers = ['A', '2', '3', '4', '5', '9', '10', 'J','Q','K'];
 
   let randomSuit = suits[Math.floor(Math.random()* suits.length)];
   let randomNumber = numbers[Math.floor(Math.random()* numbers.length)];
 
-    document.getElementById('top-suit').textContent = randomSuit;
-    document.getElementById('card-number').textContent = randomNumber;
-    document.getElementById('bottom-suit').textContent = randomSuit;
-};
+  const card = document.querySelector('.card');
+  card.style.width = '100px';
+  card.style.height = '150px';
 
-window.onload = function() {
-    console.log("");
-    newCard();
+  const topSuit = document.getElementById('top-suit');
+  const bottomSuit = document.getElementById('bottom-suit');
+  const cardNumber = document.getElementById('card-number');
 
+  topSuit.textContent = randomSuit;
+  cardNumber.textContent = randomNumber;
+  bottomSuit.textContent = randomSuit;
+
+  
+  cardNumber.style.display = 'flex';
+  cardNumber.style.justifyContent = 'center';
+  cardNumber.style.alignItems = 'center';
+  cardNumber.style.height = '100%';
+  cardNumber.style.width = '100%';
+  cardNumber.style.textAlign = 'center';
+
+  if (randomSuit === '♥' || randomSuit === '♦') {
+    topSuit.style.color = 'red';
+    bottomSuit.style.color = 'red';
+  } else {
+    topSuit.style.color = '';
+    bottomSuit.style.color = '';
+  }
 };
 setInterval(newCard, 1000);
